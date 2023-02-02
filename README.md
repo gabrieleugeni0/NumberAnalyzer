a0641077069425bae84958ec606d7bf4026beec1
Gabriel Rodrigues <gabrieleugenio90@gmail.com>
2023-02-02 19:14:16 -0300

    final commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Number Analyzer
+
+The user can add how many number he/she wants and after that require a simple analyze of this set of numbers.
+
+This set is saved in an array.
+
+The analysis contain:
+- How many number the user added
+- What is the biggest number in the array
+- What is the smallest number in the array
+- The sum of all numbers
+- The average of the set of numbers
+
+In addition, the script contain a simple validation of inputs.
+
+The user can add more numbers after analysis to make new one or clean the set, to make other analysis.
+
+####To run the script it's only run the `html.html` in a browser.
---
+++ script.js
@@ -0,0 +1,56 @@
+var arr = []
+
+function adicionar() {
+    let txtnum = document.getElementById('txtn')
+    let num = Number(txtnum.value)
+    let sel = document.getElementById('sel')
+    let res = document.getElementById('res') 
+    if (num >= 1 && num <= 100) {
+        if (arr.indexOf(num) == -1) {
+            res.innerHTML = ''
+            arr.push(num)
+            sel.options[sel.options.length] = new Option(
+                `Valor ${num} adicionado.`,`op${sel.options.length}`)
+        } else {
+            window.alert('[ERRO] Valor ja encontrado na lista!')
+        }
+    } else {
+        window.alert('[ERRO] Valor invalido! Digite um numero entre 1 e 100!')
+    }
+}
+
+function analisar() {
+    if (arr.length == 0) {
+        window.alert('[ERRO] Adicione valores antes de finalizar!')
+    } else {
+        let res = document.getElementById('res')
+        let maior = arr[0]
+        let menor = arr[0]
+        let soma = 0
+        for (let i in arr) {
+            if (arr[i] > maior) {
+                maior = arr[i]
+            }
+            if (arr[i] < menor) {
+                menor = arr[i]
+            }
+            soma += arr[i]
+        }
+        let media = (soma / (arr.length))
+        res.innerHTML += `Temos ao todo ${arr.length} valores cadastrados.<br><br><br>`
+        res.innerHTML += `O maior valor informado foi ${maior}.<br><br><br>`
+        res.innerHTML += `O menor valor informado foi ${menor}.<br><br><br>`
+        res.innerHTML += `A soma de todos os valores informados vale ${soma}.<br><br><br>`
+        res.innerHTML += `A media dos valores informados vale ${media}.`
+        }
+}
+
+function recomecar() {
+    arr = []
+    let num = document.getElementById('txtn')
+    let sel = document.getElementById('sel')
+    let res = document.getElementById('res')
+    num.value = ''
+    sel.options.length = 0
+    res.innerHTML = ''
+}
